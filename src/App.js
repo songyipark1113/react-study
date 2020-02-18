0067e8815e76269f3c4d9524f3ec7a2362b37433
songyipark1113 <ssongpu@gmail.com>
2020-02-19 04:28:00 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -13,16 +13,29 @@ function App() {
   );
 }
 
-export default App;*/
+export default App;
+*/
 
 import React,{Component} from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
+const customer = {
+  'name':'홍길동',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생'
+}
+
 class App extends Component{
   render(){
     return(
-      <Customer/>
+      <Customer
+       name={customer.name}
+       birthday={customer.birthday}
+       gender={customer.gender}
+       job={customer.job}
+      />
     );
   }
 }
