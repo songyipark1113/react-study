bc50617ee50cbdc4d806b2daf95292049a44e346
songyipark1113 <ssongpu@gmail.com>
2020-02-19 23:19:11 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ import Customer from './components/Customer'
 import './App.css';
 
 const customer = {
+  'id':1,
+  'image':'https://placeimg.com/64/64/any',
   'name':'홍길동',
   'birthday':'961222',
   'gender':'남자',
@@ -31,6 +33,8 @@ class App extends Component{
   render(){
     return(
       <Customer
+       id={customer.id}
+       image={customer.image}
        name={customer.name}
        birthday={customer.birthday}
        gender={customer.gender}
